da5b5e4e9570812c5d3ef0139c0830f91af49f4f
Mateus Soares <mateus.soares_2013@hotmail.com>
2023-11-14 14:09:08 -0300

    estruturando um projeto com handlebars

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+
+app.engine('handlebars' , exphbs.engine())
+app.set('view engine' , 'handlebars')
+
+app.use(express.static("public"))
+
+app.get('/' , (requisiçao, resposta) => {
+    resposta.render('home')
+})
+
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
